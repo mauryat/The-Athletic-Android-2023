5b5ce4bb71945125464f77d7e73514f2c43bcf58
Maurya Talisetti <mauryat@users.noreply.github.com>
2023-05-24 11:04:45 -0500

    Author data model

---
+++ app/src/main/java/com/theathletic/interview/authors/data/Author.kt
@@ -0,0 +1,12 @@
+package com.theathletic.interview.authors.data
+
+data class Author(
+    val name: String,
+    val shortname: String,
+    val deletedAt: String,
+    val updatedAt: String,
+    val title: String,
+    val imageUrlString: String,
+    val createdAt: String,
+    val id: String
+)
