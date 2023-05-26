b0467c452b6d958ce190aac5fec6c7f9bbff92c9
Maurya Talisetti <mauryat@users.noreply.github.com>
2023-05-26 17:06:08 -0500

    Remove unnecessary destinations file

--- app/src/main/java/com/theathletic/interview/main/AthleticDestinations.kt
+++
@@ -1,9 +0,0 @@
-package com.theathletic.interview.main
-
-interface AthleticDestinations {
-    val route: String
-}
-
-object Articles : AthleticDestinations {
-    override val route = "articles"
-}
